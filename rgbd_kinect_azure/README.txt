e1c7edad8d9449b363be45dc2ae3510d611119e3
boyang-liu <64985036+boyang-liu@users.noreply.github.com>
2021-05-21 09:21:21 +0200

    Add files via upload

--- rgbd_kinect_azure/README.txt
+++ rgbd_kinect_azure/README.txt
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 make sure AZURE_KINECT_SDK is set to the azure kinect sdk directory
 for 1.4.1 C:\Program Files\Azure Kinect SDK v1.4.1\sdk
+=======
+make sure AZURE_KINECT_SDK is set to the azure kinect sdk directory
+for 1.4.1 C:\Program Files\Azure Kinect SDK v1.4.1\sdk
+>>>>>>> 93b023a27477dc3599d414a52abd32b98ef97cea
 add %AZURE_KINECT_SDK%\windows-desktop\x86\release\bin to PATH
--- rgbd_kinect_azure/rgbd_frame_set.h
+++ rgbd_kinect_azure/rgbd_frame_set.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 #include <type_traits>
 #include <rgbd_device.h>
@@ -28,4 +29,36 @@ namespace rgbd_utils {
 		frame_tuple<S> frames;
 		rgbd::IMU_measurement imu_data;
 	};
+=======
+#pragma once
+#include <type_traits>
+#include <rgbd_device.h>
+
+namespace rgbd_utils {
+	
+	template<unsigned S=3>
+	struct frame_tuple{
+		enum {
+			F_COLOR = 0,
+			F_DEPTH = 1,
+			F_IR = 2,
+		};
+
+		rgbd::frame_type frame[S];
+		bool read_frame[S];
+		
+		template<unsigned F>
+		rgbd::frame_type get_frame() {
+			read_frame[F] = true;
+			return frame[F];
+		}
+	};
+
+
+	template<unsigned S = 3>
+	struct rgbd_camera_state {
+		frame_tuple<S> frames;
+		rgbd::IMU_measurement imu_data;
+	};
+>>>>>>> 93b023a27477dc3599d414a52abd32b98ef97cea
 }
