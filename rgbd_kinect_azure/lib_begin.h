6e62ff701bdc65d9f71b69856ce74b91f730fb33
boyang-liu <64985036+boyang-liu@users.noreply.github.com>
2021-05-21 09:40:28 +0200

    Add files via upload

--- rgbd_kinect_azure/lib_begin.h
+++ rgbd_kinect_azure/lib_begin.h
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 #ifdef RGBD_KINECT_AZURE_EXPORTS
 #	define CGV_EXPORTS
 #endif
 
 #include <cgv/config/lib_begin.h>
-=======
-#ifdef RGBD_KINECT_AZURE_EXPORTS
-#	define CGV_EXPORTS
-#endif
-
-#include <cgv/config/lib_begin.h>
->>>>>>> 93b023a27477dc3599d414a52abd32b98ef97cea
